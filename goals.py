2499f9645325cfca9ea61f04f5e3c7bf1b7b1451
furtadobb <furtadobb@gmail.com>
2019-11-17 14:30:52 -0300

    check_goals

--- goals.py
+++ goals.py
@@ -1,6 +1,9 @@
-''' #  https://boardgamegeek.com/thread/60929/mission-cards
+"""   https://boardgamegeek.com/thread/60929/mission-cards
 
-i) Kill a certain colour. There are 6 of these, one for each colour. If fewer than 6 colours are going to be used in the game then the cards corresponding to the unused colours are removed from the deck before missions are given out. (This is quite clearly stated in at least some versions of the rules, and failing to do so doesn't make much sense.) If you draw your own colour, then your mission changes to mission (iv).
+i) Kill a certain colour. There are 6 of these, one for each colour. If fewer than 6 colours are going to be used in
+the game then the cards corresponding to the unused colours are removed from the deck before missions are given out.
+(This is quite clearly stated in at least some versions of the rules, and failing to do so doesn't make much sense.)
+If you draw your own colour, then your mission changes to mission (iv).
 
 ii) Conquer some combination of continents. There are 6 of these:
 
@@ -13,7 +16,7 @@ Conquer Europe and Australasia and a 3rd continent of your choice [this mission 
 
 iii) Occupy 18 territories with at least 2 armies in each territory.
 
-iv) Occupy 24 territories (no restriction to 2 or more armies in each). '''
+iv) Occupy 24 territories (no restriction to 2 or more armies in each). """
 
 from countries import load_data, data
 
@@ -24,14 +27,33 @@ continent_goals = [['Asia', 'South America'], ['Asia', 'Africa'], ['North Americ
 class Goals:
     def __init__(self):
         self.type = None
+        self.enemy = None
         self.to_conquer = list()
 
-    def update_goal(self):
-        pass
+    def update_goal(self, enemy, countries):
+        if self.type == 'destroy':
+            self.to_conquer = [c for c in countries if c.owner == enemy]
 
-    def check_goal(self):
-        pass
-
+    def check_goal(self, player):
+        if self.type == 'territory18':
+            if len([c for c in player.my_countries if player.my_countries.army > 1]) > 17:
+                return True
+            return False
+        elif self.type == 'territory24':
+            if player.num_countries() > 23:
+                return True
+            return False
+        elif self.type == 'destroy':
+            if len(self.to_conquer) == 0:
+                return True
+            return False
+        else:
+            conquered = [c.id for c in player.my_countries]
+            to_conquer = [c for c in conquered if c in self.to_conquer]
+            if len(conquered) >= len(to_conquer):
+                return True
+            return False
+        
 
 def generate_continent_goals():
     goals = list()
@@ -44,11 +66,11 @@ def generate_continent_goals():
     return goals
 
 
-goals1 = generate_continent_goals()
 goals_types = ['continent', 'destroy', 'territory18', 'territory24']
+continent_goals_countries = generate_continent_goals()
+enemies = ['black', 'white', 'blue', 'red', 'yellow', 'green']
 
 
 if __name__ == '__main__':
     # cts = load_data()
-    for i in goals1:
-        print(len(i))
+    pass
