60f136af29db47334d8540e6476bc170cf4d04a5
furtadobb <furtadobb@gmail.com>
2019-11-18 15:27:33 -0300

    calculate armies

--- game_on.py
+++ game_on.py
@@ -3,6 +3,7 @@ from numpy import random
 from countries import World
 from goals import draw_n_goals, types, possible_enemies, Goal
 from players import Player
+from move import deploy
 
 
 def gen_world(num_players):
@@ -12,25 +13,21 @@ def gen_world(num_players):
     for i in range(num_players):
         p = Player(i)
         p.name = enemies[i]
-        while p.goal is None:
-            random.shuffle(gls)
-            g = gls.pop()
-            if g.enemy != p.name:
-                p.goal = g
-            else:
-                p.goal = Goal(random.choice(['territory18', 'territory24']))
+        random.shuffle(gls)
+        g = gls.pop()
+        if g.enemy != p.name:
+            p.goal = g
+        else:
+            p.goal = Goal(random.choice(['territory18', 'territory24']))
         w.players.append(p)
     return w
 
 
 def main(num_players):
     w = gen_world(num_players)
+    w = deploy(w)
     return w
 
 
 if __name__ == '__main__':
     w1 = main(6)
-    # Check continents
-    a = [lst.goal.to_conquer for lst in w1.players]
-    for i in a:
-        print(i)
--- players.py
+++ players.py
@@ -8,11 +8,15 @@ class Player:
         self.goal = None
         self.my_countries = list()
 
-    def add_country(self, country):
+    def add_country(self, country, army=1):
         self.my_countries.append(country)
+        country.owner = self
+        country.army = army
 
     def remove_country(self, country):
         self.my_countries.remove(country)
+        country.owner = None
+        country.army = 0
 
     def num_countries(self):
         return len(self.my_countries)
