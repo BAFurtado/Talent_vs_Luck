e7c550b8f974593da0fd9920bad3a301182aa96d
BAFurtado <furtadobb@gmail.com>
2019-12-02 18:38:13 -0300

    plot

--- strategies.py
+++ strategies.py
@@ -2,7 +2,6 @@ import pickle
 
 import matplotlib.pyplot as plt
 import pandas as pd
-import seaborn as sns
 
 import game_on
 
@@ -17,12 +16,14 @@ def statistics(n):
     return output
 
 
-def plotting(data, col1='o_avg_dice', col2='w_avg_dice', choice='strategy'):
+def plotting(data, col1='w_avg_dice', col2='o_avg_dice', choice='strategy'):
     fig, ax = plt.subplots()
-    colors = {'minimalist': 'red', 'random': 'green', 'blitz': 'blue'}
+    colors = {'blitz': 'blue', 'minimalist': 'red', 'random': 'green'}
     for key in colors.keys():
         ax.scatter(x=col1, y=col2, c=colors[key],
-                   data=data.loc[c[choice] == key], alpha=.5, marker='.', label=key)
+                   data=data.loc[c[choice] == key], alpha=.12, marker='.', label=key)
+
+    ax.plot([0, , [0, 0], c='black', alpha=.9)
     ax.legend(frameon=False)
 
     ls = [x.replace('_', ' ').replace('o', "Other players'").replace('w', 'Winner').replace('avg', 'average')
@@ -43,17 +44,24 @@ def plotting(data, col1='o_avg_dice', col2='w_avg_dice', choice='strategy'):
     plt.show()
 
 
+def summary(data):
+    data.groupby(by=['strategy', 'goal', 'tie']).agg(['mean', 'count']).to_csv('summary.csv', sep=';')
+
+
 if __name__ == '__main__':
     m = 10000
-    # out = statistics(m)
-    # with open(f'results/objects_{m}', 'wb') as f:
-    #     pickle.dump(out, f)
-    # print(out)
-    #
-    with open(f'results/objects_{m}', 'rb') as f:
-        c = pickle.load(f)
-    print(c)
+    generate = False
+    if generate:
+        c = statistics(m)
+        with open(f'results/objects_{m}', 'wb') as f:
+            pickle.dump(c, f)
+        print(c)
+    else:
+        with open(f'results/objects_{m}', 'rb') as f:
+            c = pickle.load(f)
+        print(c)
     c[['n_countries', 'o_avg_dice', 'w_avg_dice', 'w_num_rolls', 'o_avg_num_rolls']] = \
         c[['n_countries', 'o_avg_dice', 'w_avg_dice', 'w_num_rolls', 'o_avg_num_rolls']].astype('float')
     plotting(c)
+    summary(c)
 
