5891eb2628eba92136c4efe47dac663fff89f117
furtadobb <furtadobb@gmail.com>
2019-12-08 15:54:29 -0300

    second

--- simulation.py
+++ simulation.py
@@ -7,7 +7,8 @@ import game_on
 
 def statistics(n):
     output = pd.DataFrame(columns=['tie', 'strategy', 'goal', 'n_countries', 'o_avg_dice',
-                                   'w_avg_dice', 'w_num_rolls', 'o_avg_num_rolls'])
+                                   'w_avg_dice', 'w_num_rolls', 'o_avg_num_rolls',
+                                   '2nd_avg_dice', '2nd_num_rolls'])
     for i in range(n):
         print(f'Game {i}')
         # Results of game_on come as a dictionary
@@ -20,10 +21,10 @@ def summary(data):
     data = data.reset_index()
     data.columns = data.columns.droplevel(1)
     d1 = data.iloc[:, :5]
-    d2 = data.iloc[:, [5, 7, 9, 11]]
+    d2 = data.iloc[:, [5, 7, 9, 11, 12, 13]]
     data = pd.concat([d1, d2], axis=1)
-    data.columns = ['strategy', 'goal', 'tie', 'n_countries', 'num_wins', 'o_avg_dice',
-                    'w_avg_dice', 'w_num_rolls', 'o_avg_num_rolls']
+    data.columns = ['strategy', 'goal', 'tie', 'n_countries', 'num_wins', 'o_avg_dice', 'w_avg_dice', 'w_num_rolls',
+                    'o_avg_num_rolls', '2nd_avg_dice', '2nd_num_rolls']
     data.to_csv('summary.csv', sep=';', index=False)
     return data
 
@@ -36,14 +37,15 @@ def main(n=10000, generate=True):
     else:
         with open(f'results/objects_{n}', 'rb') as f:
             c = pickle.load(f)
-    c[['n_countries', 'o_avg_dice', 'w_avg_dice', 'w_num_rolls', 'o_avg_num_rolls']] = \
-        c[['n_countries', 'o_avg_dice', 'w_avg_dice', 'w_num_rolls', 'o_avg_num_rolls']].astype('float')
+    c[['n_countries', 'o_avg_dice', 'w_avg_dice', 'w_num_rolls', 'o_avg_num_rolls', '2nd_avg_dice', '2nd_num_rolls']] = \
+        c[['n_countries', 'o_avg_dice', 'w_avg_dice', 'w_num_rolls', 'o_avg_num_rolls', '2nd_avg_dice',
+           '2nd_num_rolls']].astype('float')
     s = summary(c)
     print(s)
     return c
 
 
 if __name__ == '__main__':
-    m = 100
+    m = 10
     gen = False
     o = main(m, gen)
